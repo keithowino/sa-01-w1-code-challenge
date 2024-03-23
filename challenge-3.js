92c70c1fe84ce57b14488b308f045d08ff30ff3a
keithowino <designsolutions1629@gmail.com>
2024-03-24 01:25:53 +0300

    Commence build PSEUDOCODE for challenge 3.

--- challenge-3.js
+++ challenge-3.js
@@ -1,9 +1,22 @@
+
+// * NET SALARY CALCULATOR
+
+// * Goal
+
 /*
-Challenge 3: Net Salary Calculator (Toy Problem)
 Write a program whose major task is to calculate an individual’s Net Salary by getting the inputs of basic salary and benefits. Calculate the payee (i.e. Tax), NHIFDeductions, NSSFDeductions, gross salary, and net salary. 
 NB: Use KRA, NHIF, and NSSF values provided in the link below.
 
 https://www.aren.co.ke/payroll/taxrates.htmLinks to an external site.  
 
 https://www.kra.go.ke/en/individual/calculate-tax/calculating-tax/payeLinks to an external site.
+*/
+
+let prompt = require("prompt-sync")({sigint: true});
+
+/*
+TODO:- PSEUDOCODE
+
+- calculate an individual’s Net Salary
+
 */
