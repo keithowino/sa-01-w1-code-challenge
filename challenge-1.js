c9278fd6ee5f2457a06347070372e25f69881d9f
keithowino <designsolutions1629@gmail.com>
2024-03-24 01:17:10 +0300

    Modify challenge 1.

--- challenge-1.js
+++ challenge-1.js
@@ -1,17 +1,23 @@
+
+// * STUDENT GRADE GENERATOR
+
+// * Goal
+
 /*
-* STUDENT GRADE GENERATOR.
-* Write a program that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
-  A > 79, B - 60 to 79, C -  49 to 59, D - 40 to 49, E - less 40.
+Write a program that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
+
+A > 79, B - 60 to 79, C -  49 to 59, D - 40 to 49, E - less 40.
 */
 
 const prompt = require("prompt-sync")({sigint: true});
 
-const marksInput = parseInt(prompt("Input student marks? "));
+const marksInput = parseInt(prompt("Input student marks? "));    // * Collect marks.
 
 function grader(marks){
   let div = "------\n";
-  let output;
+  let output;    // * This is the value that the function will return.
 
+  // * Now this if statement block determines which grade a particular mark will get.
   if(marks > 79 && marks <= 100){
     output = "A";
   }else if(marks > 59 && marks <= 79){
@@ -23,7 +29,7 @@ function grader(marks){
   }else if(marks >= 0 && marks <= 39){
     output = "E";
   }else{
-    output = "Marks entered are invalid"
+    output = "Marks entered are invalid";
   };
 
   return div + output;
