7774c5af5497e8f00f5f9373644a0a8922a9d1b3
keithowino <designsolutions1629@gmail.com>
2024-03-24 23:21:51 +0300

    Modify challenge 1.

--- challenge-1.js
+++ challenge-1.js
@@ -1,4 +1,53 @@
 
+// // * STUDENT GRADE GENERATOR
+
+// // * Goal
+
+// /*
+// Write a program that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
+
+// A > 79, B - 60 to 79, C -  49 to 59, D - 40 to 49, E - less 40.
+// */
+
+// const prompt = require("prompt-sync")({sigint: true});
+
+// const marksInput = parseInt(prompt("Input student marks? "));    // * Collect marks.
+
+// function grader(marks){
+//   let div = "------\n";
+//   let output;    // * This is the value that the function will return.
+
+//   // * Now this if statement block determines which grade a particular mark will get.
+//   if(marks > 79 && marks <= 100){
+//     output = "A";
+//   }else if(marks > 59 && marks <= 79){
+//     output = "B";
+//   }else if(marks > 49 && marks <= 59){
+//     output = "C";
+//   }else if(marks > 39 && marks <= 49){
+//     output = "D";
+//   }else if(marks >= 0 && marks <= 39){
+//     output = "E";
+//   }else{
+//     output = "Marks entered are invalid";
+//   };
+
+//   return div + output;
+// };
+
+// let result = grader(marksInput);
+
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
 // * STUDENT GRADE GENERATOR
 
 // * Goal
@@ -17,17 +66,19 @@ function grader(marks){
   let div = "------\n";
   let output;    // * This is the value that the function will return.
 
-  // * Now this if statement block determines which grade a particular mark will get.
-  if(marks > 79 && marks <= 100){
-    output = "A";
-  }else if(marks > 59 && marks <= 79){
-    output = "B";
-  }else if(marks > 49 && marks <= 59){
-    output = "C";
-  }else if(marks > 39 && marks <= 49){
-    output = "D";
-  }else if(marks >= 0 && marks <= 39){
-    output = "E";
+  if(!isNaN(marksInput)){
+    // * Now this if statement block determines which grade a particular mark will get.
+    if(marks > 79 && marks <= 100){
+      output = "A";
+    }else if(marks > 59 && marks <= 79){
+      output = "B";
+    }else if(marks > 49 && marks <= 59){
+      output = "C";
+    }else if(marks > 39 && marks <= 49){
+      output = "D";
+    }else if(marks >= 0 && marks <= 39){
+      output = "E";
+    }
   }else{
     output = "Marks entered are invalid";
   };
